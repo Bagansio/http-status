625c07b169aa0618f19eb68368ff8d51d6459512
Bagansio <alexmoaleal.5@gmail.com>
2024-01-08 11:45:59 +0000

    Add logging

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, Response
+from flask import Flask
+import logging
+
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)  # Set log level to INFO
 
 @app.route("/status/<status_code>")
 def status(status_code):
+    app.logger.info(f"Received request for status code {status_code}")
+
     return f"Code {status_code}", status_code
 
 if __name__ == "__main__":
-  #    app = create_app()
-  print(" Starting app...")
-  app.run(host="0.0.0.0", port=5000)
+    print("Starting app...")
+    app.run(host="0.0.0.0", port=5000)
